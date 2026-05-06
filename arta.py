1924762033df5a66bb6adfd01ffff459475b6900
Narton44 <sosokom5522@gmaol.com>
2026-05-06 17:14:47 +1100

    second

--- arta.py
+++ arta.py
@@ -1,48 +1,101 @@
 from tkinter import *
+import math
 
 root = Tk()
 
-root.iconbitmap('mortar_81.ico')
 root.title('АРТА')
 root.geometry('777x777')
 root.resizable(1, 1)
 root['bg'] = 'green'
 
-label = Label(
+
+
+# ----------------------Прямая геодезическая задача----------------------
+
+label_x = Label(
     root,
-    text='Огонь из 120!',
-    font=('Comic Sans MS', 20, 'bold'),
-    bg='brown',
+    text='Х позиции',
     width=11,
     )
+label_x.place(x=0,y=0)
 
-label.pack()
-label.place(x=0,y=0, anchor='nw')
-
-def big_click():
-    print('БАДА БУМ!!!')
-
-btn = Button(
+label_y = Label(
     root,
-    text='Огонь из 120!',
-    command=big_click,
-    font=('Comic Sans MS', 20, 'bold'),
-    bg='lime',
-    activebackground='red',
-    activeforeground='blue',
+    text='Y позиции',
     width=11,
-    cursor='hand2',
     )
+label_y.place(x=0,y=50)
 
-btn.pack()
-btn.place(x=0,y=43)
+label_d = Label(
+    root,
+    text='Дистанция',
+    width=11,
+    )
+label_d.place(x=0,y=90)
+
+label_angle = Label(
+    root,
+    text='Дирекц. угол',
+    width=11,
+    )
+label_angle.place(x=0,y=135)
+
+label_aim_coords = Label(
+    root,
+    text=' ',
+    width=27,
+    height=2,
+    )
+label_aim_coords.place(x=0,y=250)
 
 
 position_x = Entry(
     root,
-    text='Х позиции'
 )
-position_x.place(x=100,y=200)
+# position_x.insert(0, "Х позиции")
+position_x.place(x=0,y=20)
+
+position_y = Entry(
+    root,
+)
+# position_y.insert(0, "Y позиции")
+position_y.place(x=0,y=65)
+
+distance = Entry(
+    root,
+)
+# distance.insert(0, "Дистанция")
+distance.place(x=0,y=110)
+
+dir_angle = Entry(
+    root,
+)
+# angle.insert(0, "Дистанция")
+dir_angle.place(x=0,y=155)
+
+
+def find_aim_x_y(): # сам метод расчета коодинат цели
+
+    pos_x=float(position_x.get())
+    pos_y=float(position_y.get())
+    dist=float(distance.get())
+    dir_ang=math.radians(float(dir_angle.get()))
+    # dir_ang_rad = math.radians(dir_ang)  # Обязательно!
+
+    aim_x = pos_x + dist * math.cos(dir_ang)
+    aim_y = pos_y + dist * math.sin(dir_ang)
+    label_aim_coords.configure(text=f'X цели={aim_x}, Y цели={aim_y}')
+
+
+btn = Button( # кнопка запуска расчета коодинат цели
+    root,
+    text='Рассчитать координаты цели',
+    command=find_aim_x_y,
+    font=('Comic Sans MS', 10, 'bold'),
+    cursor='hand2',
+    )
+btn.place(x=0,y=180)
+
 
 
 mainloop()
